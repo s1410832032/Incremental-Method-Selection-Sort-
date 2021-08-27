d72afdd9a7d120181415518362b3767db6c37b61
Yumi Chen <66152558+s1410832032@users.noreply.github.com>
2021-08-27 03:19:37 +0000

    selection_sort0827

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+using namespace std;
+
+void selection_sort(int array[],int n){
+  for (int i=0 ; i<n ; i++){
+    int min_index=i;
+    for(int j=i+1 ; j<n ; j++)
+      if(array[min_index]>array[j])
+        min_index=j;
+      swap(array[min_index],array[i]);
+  }
+  for(int i=0 ; i<n ; i++)
+    cout << array[i];
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  int array[5]={2,5,7,9,4};
+
+  for (int i=0;i<5;i++){
+    int min_index=i;
+    for(int j=i+1;j<5;j++)
+      if(array[min_index]>array[j])
+        min_index=j;
+        swap(array[i],array[min_index]);
+  }
+  for (int i=0 ; i< 5 ; i++)
+    cout << array[i] ;
+  cout << "\n";
+  selection_sort(array, 5);
 }
